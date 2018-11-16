f6fa0ea610fab09659780c2b2b25f2e4c78b9897
pactera-admindev-lizhe <zhe.li4@pactera.com>
2018-11-16 17:48:38 +0800

    added session

--- app/app.js
+++ app/app.js
@@ -8,8 +8,18 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var ejs = require("ejs");
-
+var session = require("express-session");
 var app = express();
+app.use(
+  session({
+    secret: "secret", // 对session id 相关的cookie 进行签名
+    resave: true,
+    saveUninitialized: false, // 是否保存未初始化的会话
+    cookie: {
+      maxAge: 1000 * 60 * 3 // 设置 session 的有效时间，单位毫秒
+    }
+  })
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
